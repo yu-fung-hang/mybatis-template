de127c2a525a44013de407642880db706d826e8f
sing-fung <1587898817@qq.com>
2024-12-22 14:56:11 -0500

    Initial commit

---
+++ src/main/java/com/yufung/demo/MybatisTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.yufung.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisTemplateApplication.class, args);
+    }
+
+}
