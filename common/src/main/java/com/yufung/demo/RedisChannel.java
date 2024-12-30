1ee24ff6d97dd9cd3604a49716ffa4b51d267837
sing-fung <1587898817@qq.com>
2024-12-30 00:03:44 -0500

    remove RedisChannel

--- common/src/main/java/com/yufung/demo/RedisChannel.java
+++ common/src/main/java/com/yufung/demo/RedisChannel.java
@@ -1,20 +1,20 @@
-package com.yufung.demo;
-
-public class RedisChannel {
-
-
-    /**
-     * 敏感词消息
-     */
-    public final static String SENSITIVE_WORDS = "SENSITIVE_WORDS";
-
-    /**
-     * 用户禁用消息（管理员、卖家，买家）
-     * 应用收到此消息后，应时该用户退出
-     */
-    public final static String USER_DISABLE = "USER_DISABLE";
-
-
-
-
-}
+//package com.yufung.demo;
+//
+//public class RedisChannel {
+//
+//
+//    /**
+//     * 敏感词消息
+//     */
+//    public final static String SENSITIVE_WORDS = "SENSITIVE_WORDS";
+//
+//    /**
+//     * 用户禁用消息（管理员、卖家，买家）
+//     * 应用收到此消息后，应时该用户退出
+//     */
+//    public final static String USER_DISABLE = "USER_DISABLE";
+//
+//
+//
+//
+//}
